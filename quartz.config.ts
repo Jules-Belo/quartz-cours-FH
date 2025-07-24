e4edd91fae6fc569f677e39313a5c4d0e3f447f4
Jules-Belo <jules.belo@icloud.com>
2025-07-24 14:15:43 +0200

    fix: baseUrl mis à jour pour GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,8 +15,8 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "fr-FR",
+    baseUrl: "/quartz-cours-FH",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
